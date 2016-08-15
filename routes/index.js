9fd0533724988277fc41b4bb20ebbe4c1771cce2
Danilo <dbq.batista@gmail.com>
2016-08-15 00:12:11 -0300

    nodejs with express, mustache-express, morgan, mongoose, mongodb, twitter-bootstrap, jquery

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/navbars', function(req, res, next) {
+    res.render('navbars1', { title: 'Barra de menu'});
+});
+router.get('/content', function(req, res, next) {
+    res.render('content', { title: 'ehehe'});
+});
+router.get('/masterpage', function(req, res, next) {
+    console.log('pagina masterpage chamada');
+    res.render('masterpage', {
+        head: {
+              title: 'page title'
+        },
+        content: {
+              title: 'post title',
+              description: 'description'
+        }
+    });
+});
+exports.findAll = function(req, res) {
+    var mongodb = require('mongodb');
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://localhost:27017/netimoveis';
+    MongoClient.connect(url, function (err, db) {
+      if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+        return;
+      } else {
+        console.log('Connection established to', url);
+        var collectionUsers = db.collection('users');
+        //console.log(collectionUsers);
+        //db.collection.find({})
+        var filterRowStatus = { roles: 'user' };
+        var sort = { name: -1 };
+        collectionUsers.find(filterRowStatus).sort(sort).toArray(function(err, items) {
+            console.log(items);
+            res.render('master', {
+                head: {
+                    title: 'Your title here'
+                },
+                homepage: {
+                    title: 'home sweet home',
+                    items: items
+                }
+            });
+        });
+        db.close();
+    }
+  });
+};
+router.get('/',  exports.findAll);
+
+module.exports = router;
